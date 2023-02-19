1e24fc98c6d69897538218523d2c7adde0b3f3e1
Austin Steffes <austinjsteffes@gmail.com>
2023-02-19 12:07:49 -0600

    Lesson 05 completed

--- Lesson_05/project05-3/scripts/project05-03.js
+++ Lesson_05/project05-3/scripts/project05-03.js
@@ -10,8 +10,31 @@
       Filename: project05-03.js
 */
 
+//Creating initial variables here.
 let sourceDoc = document.getElementById("source_doc");
 let tOc = document.getElementById("toc");
 let headingCount = 1;
-const heading = "H2";
+const HEADING = "H2";
 
+//The for loop here going from first child element, through last child then finally to null.
+for (let childNode = sourceDoc.firstElementChild; childNode !== null; childNode = childNode.nextElementSibling)
+{
+      //Figuring out if the childNode is equal to the value of the heading constant.
+      if(childNode.nodeName === HEADING)
+      {
+            //More variables, and using the childNode and inserting.
+            let anchor = document.createElement("a");
+            anchor.name = "doclink" + headingCount;
+            childNode.insertBefore(anchor, childNode.firstElementChild);
+
+            let listItem = document.createElement("li");
+            let link = document.createElement("a");
+
+            listItem.appendChild(link);
+            link.textContent = childNode.textContent;
+
+            link.href = "#doclink" + headingCount;
+            tOc.appendChild(listItem);
+            headingCount++;
+      }
+}
---
+++ Lesson_05/project05-4/scripts/project05-04.js
@@ -0,0 +1,41 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-04
+
+      Project to display footnotes in a popup window
+      Author: austin steffes
+      Date:   2/18
+
+      Filename: project05-04.js
+*/
+
+// Node list of phrases that are associated with footnotes
+let phrases = document.querySelectorAll("article blockquote dfn");
+
+for (let i = 0; i < phrases.length; i++) 
+{
+            phrases[i].onclick = (() => {
+            let phrase = document.createElement("h1");
+            phrase.textContent = this.textContent;
+
+            let footnote = document.createElement("p");
+            footnote.textContent = footnotes[i];
+            footnote.style.fontStyle = 'italic';
+            footnote.style.fontSize = '1.2em';
+
+            let closeButton = document.createElement("input");
+            closeButton.type = "button";
+            closeButton.value = "Close Footnote";
+            closeButton.style = "display: block; margin: 10px auto";
+
+            let popup = window.open("", "Footnote", "width=300,height=200,top=100,left=100");
+            let popUpBody = popup.document.body;
+            popUpBody.style =  "background-color: ivory; font-size: 16px; padding: 10px;";
+            popUpBody.appendChild(phrase);
+            popUpBody.appendChild(footnote);
+            popUpBody.appendChild(closeButton);
+            console.log(closeButton);
+            closeButton.onclick = (() => {popup.close();});
+      })
+}
