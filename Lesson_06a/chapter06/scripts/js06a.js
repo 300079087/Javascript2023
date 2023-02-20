6f532d3409f08cfabb13af08e399eae31cbbbb80
Austin Steffes <austinjsteffes@gmail.com>
2023-02-20 14:40:52 -0600

    push from laptop, pull onto desktop

---
+++ Lesson_06a/chapter06/scripts/js06a.js
@@ -0,0 +1,24 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Chapter case
+
+      Order Form Code
+     Author: austin steffes
+      Date:   2/20/23  
+
+      Filename: js06a.js
+ */
+
+window.addEventListener("load", fucntion())
+{
+      let orderForm = document.forms.orderForm;
+      let model = orderForm.elements.model;
+
+      //select model selection list when form opens. focus your mouse/attention.
+      model.focus();
+
+      //creating another function inside the eventlistener.
+      
+}
+
