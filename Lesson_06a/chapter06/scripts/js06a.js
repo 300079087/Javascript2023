42e2945c7f938f364d7fcbf2ca9d837a8b725472
Steffes <300079087@fvtc.edu>
2023-02-20 15:41:31 -0600

    chapter is done

--- Lesson_06a/chapter06/scripts/js06a.js
+++ Lesson_06a/chapter06/scripts/js06a.js
@@ -10,15 +10,51 @@
       Filename: js06a.js
  */
 
-window.addEventListener("load", fucntion())
-{
+window.addEventListener("load", function(){
       let orderForm = document.forms.orderForm;
       let model = orderForm.elements.model;
 
       //select model selection list when form opens. focus your mouse/attention.
       model.focus();
 
+      //add event listener for every form element.
+      for (let i = 0; i <  orderForm.elements.length; i++)
+      {
+            orderForm.elements[i].addEventListener("change", calcOrder); 
+      }
+
+      //calling function.
+      calcOrder();
+
       //creating another function inside the eventlistener.
-      
-}
+      function calcOrder()
+      { 
+            let mIndex = model.selectedIndex;
+            let mValue = model.options[mIndex].value;
+
+            let qIndex = orderForm.elements.qty.selectedIndex;
+            let quantity = orderForm.elements.qty[qIndex].value;
+
+            let modelCost = mValue * quantity;
+            orderForm.elements.modelCost.value = modelCost.toLocaleString("en-US", {style: "currency", currency: "USD"});
+
+            let planValue = document.querySelector('input[name="plan"]:checked').value;
+
+            let planCost = planValue * quantity;
+            orderForm.elements.planCost.value = planCost.toLocaleString("en-US", {style: "currency", currency: "USD"});
+
+            let subtotal = modelCost + planCost;
+            orderForm.elements.subtotal.value = subtotal.toLocaleString("en-US", {style: "currency", currency: "USD"});
+
+            let salesTax = subtotal * .05;
+            orderForm.elements.salesTax.value = salesTax.toLocaleString("en-US", {style: "currency", currency: "USD"});
+
+            let totalCost = subtotal + salesTax;
+            orderForm.elements.totalcost.value = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"});
+
+            orderForm.elements.modelName.value = model.options[mIndex].text;
+            let selectedPlan = document.querySelector('input[name="plan"]:checked');
+            orderForm.elements.planName.value = selectedPlan.labels[0].textContent;
+      }
+}) 
 
